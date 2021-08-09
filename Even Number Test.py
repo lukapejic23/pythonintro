a0f434d84561c931c51573d895c724e82c733a38
lukapejic23 <85244179+lukapejic23@users.noreply.github.com>
2021-08-09 22:25:37 +0200

    Add files via upload

---
+++ Even Number Test.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))  
+if (num % 2) == 0:  
+   print("True".format(num))  
+else:  
+   print("False".format(num))  
+
+while True: 
+	answer = input("Type x to quit: ") 
+	if answer == "x": 
+		break 
